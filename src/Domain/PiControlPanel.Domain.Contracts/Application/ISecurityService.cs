9aad5075dbcb7858b71fbd86f5e1a50166cfe300
rembertmagri <rembert.magri@gmail.com>
2020-05-29 07:56:52 -0400

    Start of contracts documentation

--- src/Domain/PiControlPanel.Domain.Contracts/Application/ISecurityService.cs
+++ src/Domain/PiControlPanel.Domain.Contracts/Application/ISecurityService.cs
@@ -3,10 +3,23 @@
     using System.Threading.Tasks;
     using PiControlPanel.Domain.Models.Authentication;
 
+    /// <summary>
+    /// Application layer service for authenticating.
+    /// </summary>
     public interface ISecurityService
     {
+        /// <summary>
+        /// Logs the user in if valid, returning the LoginResponse object.
+        /// </summary>
+        /// <param name="userAccount">The user account information.</param>
+        /// <returns>The LoginResponse object.</returns>
         Task<LoginResponse> LoginAsync(UserAccount userAccount);
 
+        /// <summary>
+        /// Refreshes the LoginResponse object.
+        /// </summary>
+        /// <param name="userAccount">The user account information.</param>
+        /// <returns>The LoginResponse object.</returns>
         Task<LoginResponse> GetLoginResponseAsync(UserAccount userAccount);
     }
 }
