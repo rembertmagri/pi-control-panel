de1ac634eba4055f4ad20a2c896ecfce1d40d84f
rembertmagri <rembert.magri@gmail.com>
2020-06-13 15:37:56 -0400

    Fixing install script

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 
 grep -Fxq "deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main" /etc/apt/sources.list
 if [ $? -ne 0 ]; then
-    wget -qO - https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo/PUBLIC.KEY | sudo apt-key add -
-    echo 'deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main' | sudo tee -a /etc/apt/sources.list
-    sudo apt-get update
-    sudo apt-get install pi-control-panel
+    wget -qO - https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo/PUBLIC.KEY | apt-key add -
+    echo 'deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main' | tee -a /etc/apt/sources.list
 else
-    echo 'Pi Control Panel is already installed. Run "apt-get update" and "apt-get upgrade" to get the latest version.'
+    apt-get update
+    apt-get install pi-control-panel
 fi
