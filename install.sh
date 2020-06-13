1414b8a63f23283c36448b59372f3c806b7bee15
rembertmagri <rembert.magri@gmail.com>
2020-06-13 15:50:19 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+gpg --list-keys F4109F88DD8DCD60 &> /dev/null
+if [ $? -ne 0 ]; then
+    wget -qO - https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo/PUBLIC.KEY | sudo apt-key add -
+fi
+
 grep -Fxq "deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main" /etc/apt/sources.list
 if [ $? -ne 0 ]; then
-    wget -qO - https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo/PUBLIC.KEY | apt-key add -
-    echo 'deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main' | tee -a /etc/apt/sources.list
-else
-    apt-get update
-    apt-get install pi-control-panel
+    echo 'deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main' | sudo tee -a /etc/apt/sources.list
 fi
+
+sudo apt-get update
+sudo apt-get install pi-control-panel
