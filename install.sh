aebeb6d1d933f7c1c2e40421ba28d093fcbc60b7
rembertmagri <rembert.magri@gmail.com>
2020-06-13 15:35:47 -0400

    Fixing install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep -Fxq "deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main" /etc/apt/sources.list &> /dev/null
+grep -Fxq "deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main" /etc/apt/sources.list
 if [ $? -ne 0 ]; then
     wget -qO - https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo/PUBLIC.KEY | sudo apt-key add -
     echo 'deb https://raw.githubusercontent.com/rembertmagri/pi-control-panel/master/debian_repo buster main' | sudo tee -a /etc/apt/sources.list
