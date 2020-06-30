11822b63413cce3c98055997b8a18dad706cb218
rembertmagri <rembert.magri@gmail.com>
2020-06-29 21:51:55 -0400

    Fixing terminal to handle input/output via stream

--- src/Application/PiControlPanel.Application.SecureShell/WebSocketDataType.cs
+++ src/Application/PiControlPanel.Application.SecureShell/WebSocketDataType.cs
@@ -13,21 +13,6 @@
         /// <summary>
         /// Dimensions type.
         /// </summary>
-        Dimensions,
-
-        /// <summary>
-        /// Command standard input type.
-        /// </summary>
-        StandardInput,
-
-        /// <summary>
-        /// Command standard output type.
-        /// </summary>
-        StandardOutput,
-
-        /// <summary>
-        /// Command standard error type.
-        /// </summary>
-        StandardError
+        Dimensions
     }
 }
